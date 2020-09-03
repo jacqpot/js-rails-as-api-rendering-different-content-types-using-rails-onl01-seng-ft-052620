d2e41ff4209f6fbe79f5c0fef137c08b15cbbeb8
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-09-03 11:36:57 -0600

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,6 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+    birds = Bird.all
+    render json: { birds: birds, messages: ['Hello Birsds', 'Goodbye Birds']}.to_json 
   end
 end
